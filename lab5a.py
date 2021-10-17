1a0f0997db29e7d010341f1271bfde8f50f17e52
Ahmed Maqsood <amaqsood7@myseneca.ca>
2021-10-16 22:52:14 -0400

    Completed

---
+++ lab5a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# Author ID: amaqsood7
+
+
+
+
+
+
+def read_file_string(file_name):
+    # Takes file_name as string for a file name, returns its entire contents as a string
+    f = open('data.txt', 'r')
+    s1 = f.read()
+    f.close()
+    return(s1) 
+    
+    
+def read_file_list(file_name):
+    # Takes a file_name as string for a file name, 
+    # return its entire contents as a list of lines without new-line characters
+    a = open('data.txt', 'r')
+    l1 = a.read().splitlines()
+    a.close()
+    return(l1)
+
+
+
+if __name__ == '__main__':
+    file_name = 'data.txt'
+    print(read_file_string(file_name))
+    print(read_file_list(file_name))
